23c5e980413866afcbfc9c66344a50cfa28161e2
Łukasz Skarżyński <skarzynski_lukasz@protonmail.com>
2018-01-23 21:38:54 +0100

    add endpoint to retrieve all tags

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,9 +1,15 @@
 from django.conf.urls import url
 
-from offers.views import OffersListView, OffersStatsView, SystemInfoView
+from offers.views import (
+    OffersListView,
+    OffersStatsView,
+    SystemInfoView,
+    tags_list_view,
+)
 
 urlpatterns = [
     url(r'^info/$', SystemInfoView.as_view(), name='info'),
     url(r'^offers/$', OffersListView.as_view(), name='offers'),
     url(r'^offers/stats/$', OffersStatsView.as_view(), name='stats'),
+    url(r'^tags/$', tags_list_view, name='tags'),
 ]
